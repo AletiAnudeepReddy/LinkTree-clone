513fe033cd6269dd0c1ee477f7f1001599d79d52
anudeep096 <aletianudeepreddy8@gmail.com>
2025-07-04 17:40:04 +0530

    nav

--- app/page.js
+++ app/page.js
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      
-    </>
+    <main>
+      <section className="bg-[#254f1a] min-h-[100vh]">
+        
+      </section>
+      <section className="bg-red-300 min-h-[100vh]">
+        
+      </section>
+    </main>
   );
 }
