68079fcaeccdda9a6746dbd65cbad081f2ee5932
anudeep096 <aletianudeepreddy8@gmail.com>
2025-07-05 19:53:19 +0530

    half

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,6 +1,36 @@
+"use client"
 import React from 'react'
+import { useState } from 'react'
+import { ToastContainer,toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Generate = () => {
+
+    const [link, setlink] = useState("")
+    const [linktext, setlinktext] = useState("")
+    const [handle, sethandle] = useState("")
+    const [pic, setpic] = useState("")
+
+    const addLink=async(link,linktext,handle)=>{
+        const myHeaders=new Headers();
+        myHeaders.append("Content-Type","application/json")
+        const raw=JSON.stringify({
+            "link":link,
+            "linktext":linktext,
+            "handle":handle
+        });
+        const requestOptions={
+            method:"POST",
+            headers:myHeaders,
+            body:raw,
+            rediret:"follow"
+        };
+
+        const r=await fetch("http://localhost:3000/api/add",requestOptions)
+        const result=await r.json()
+        toast(r.message)
+
+    }
   return (
     <>
     <div className='bg-[#e9c0e9] min-h-screen grid grid-cols-2'>
@@ -10,21 +40,21 @@ const Generate = () => {
             <div className='item'>
                 <h2 className='font-semibold text-2xl mb-1'>Step-1: Claim your Handle</h2>
                 <div className='ml-2'>
-                    <input className='px-4 bg-white py-2 mr-2 my-2 focus:outline-pink-500 rounded-full' type='text' placeholder='Choose a Handle'/>
+                    <input value={handle||""} onChange={e=>{sethandle(e.target.value)}} className='px-4 bg-white py-2 mr-2 my-2 focus:outline-pink-500 rounded-full' type='text' placeholder='Choose a Handle'/>
                 </div>
             </div>
             <div className='item'>
                 <h2 className='font-semibold text-2xl mb-1'>Step-2: Add Links</h2>
                 <div className='ml-2'>
-                    <input className='px-4 bg-white py-2 mr-2 my-2 focus:outline-pink-500 rounded-full' type='text' placeholder='Enter Link text'/>
-                    <input className='px-4 bg-white py-2 mr-2 my-2 focus:outline-pink-500 rounded-full' type='text' placeholder='Enter Link'/>
-                    <button className='p-5 py-2 ml-1 cursor-pointer hover:bg-slate-800 bg-slate-700 text-white font-bold rounded-3xl'>Add Link</button>
+                    <input value={linktext||""} onChange={e=>{setlinktext(e.target.value)}} className='px-4 bg-white py-2 mr-2 my-2 focus:outline-pink-500 rounded-full' type='text' placeholder='Enter Link text'/>
+                    <input value={link||""} onChange={e=>{setlink(e.target.value)}} className='px-4 bg-white py-2 mr-2 my-2 focus:outline-pink-500 rounded-full' type='text' placeholder='Enter Link'/>
+                    <button  className='p-5 py-2 ml-1 cursor-pointer hover:bg-slate-800 bg-slate-700 text-white font-bold rounded-3xl'>Add Link</button>
                 </div>
             </div>
             <div className='item'>
                 <h2 className='font-semibold text-2xl mb-1'>Step-3: Add picture and finalize</h2>
                 <div className='ml-2 flex flex-col'>
-                    <input className='px-4 bg-white py-2 mr-30 my-2 focus:outline-pink-500 rounded-full' type='text' placeholder='Enter Link to your picture'/>
+                    <input value={pic||""} onChange={e=>{setpic(e.target.value)}} className='px-4 bg-white py-2 mr-30 my-2 focus:outline-pink-500 rounded-full' type='text' placeholder='Enter Link to your picture'/>
                     <button className='p-5 w-fit mt-4 py-2 ml-2 cursor-pointer hover:bg-slate-800 bg-slate-700 text-white font-bold rounded-3xl'>Create your Linktree</button>
                
                 </div>
@@ -35,7 +65,7 @@ const Generate = () => {
             <img className='h-full object-contain mr-[6vw]' src='/generate.png' alt='generate'/>
         </div>
     </div>
-    <div className='bg-white min-h-[100vh]'>
+    <div className='bg-slate-100 min-h-[100vh]'>
 
     </div>
     </>
